dc1b023d726bbe75bfba47a2d93043ee240e30be
Fahad Azam <azamfahad611@gmail.com>
2020-11-24 21:03:59 +0530

    3rd day js
    will come and again to redo this
    
    a lot to learn here

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const inputs=document.querySelectorAll('.control input')
+
+function handleUpdate(){
+    // console.log(this.value)
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
